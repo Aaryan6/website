4e8a0e63cc0a0d014123b446cdbeff8e12be463d
Aaryan Patel <85422491+Aaryan6@users.noreply.github.com>
2021-07-11 07:50:44 +0530

    update setting

--- codingwebsite/settings.py
+++ codingwebsite/settings.py
@@ -25,9 +25,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-zb9%a_oh-%r^o&aqndjj00ndl3e38s6-r!_u&ceu5dta1l&6d7'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1']
 
 
 # Application definition
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
